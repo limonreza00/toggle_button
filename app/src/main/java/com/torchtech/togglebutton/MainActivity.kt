4af008d6a588147fe548896c9f3f1cbabcf8de8f
Limon <limonreza3.1416@gmail.com>
2024-03-24 17:34:56 +0600

    Custom toggle backgroud added

--- app/src/main/java/com/torchtech/togglebutton/MainActivity.kt
+++ app/src/main/java/com/torchtech/togglebutton/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.torchtech.togglebutton
 
 import android.os.Bundle
+import android.widget.TextView
+import android.widget.ToggleButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +17,13 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val toggleButton = findViewById<ToggleButton>(R.id.toggleButton)
+        val display = findViewById<TextView>(R.id.display)
+
+        toggleButton.setOnCheckedChangeListener { _, isChecked ->
+           display.text = if (isChecked) "Wifi is ON" else "Wifi is OFF"
         }
     }
 }
